1bbecb927aea051bbeaa5b3bf2c0ef591d4395b6
Richard Platel <richard.platel@gmail.com>
2023-12-04 18:01:53 -0400

    thanks strings, thrings

--- 32.rb
+++ 32.rb
@@ -1,68 +1,43 @@
 
-def is_digit?(c)
-  !!(c =~ /[0-9]/)
+def pinspect(*args)
+  puts *args.map(&:inspect)
 end
 
-def is_symbol?(c)
-  !is_digit?(c) && c != '.'
-end
 
-def search_box(row, column)
-  [-1, 0, 1].flat_map{ |y| [-1, 0, 1].map { |x| [row + x, column + y]} }.filter { |c| c[0] >= 0 && c[1] >= 0 && c[0] <= MAX_ROW && c[1] <=MAX_COL}
-end
+# [part_no, start_idx, end_idx]
 
-def is_part_no(field, row, column)
-  search_box(row, column).any?{|c| is_symbol?(field[c[0]][c[1]])}
-end
-
-field = []
+raw_field = []
 File.readlines("31.txt", chomp: true).each do |line|
-  field.append(line.split(''))
+  raw_field.append(line)
 end
 
-MAX_ROW = field.length - 1
-MAX_COL = field[0].length - 1
+# [part_no, [columns occupied]
+all_parts = raw_field.map do |row|
+  res = []
+  row.scan(/\d+/) do |part_no|
+    res << [part_no, (Regexp.last_match.offset(0)[0]...Regexp.last_match.offset(0)[1]).to_a]
+  end
+  res
+end
 
-# puts ("#{MAX_ROW}, #{MAX_COL}")
-
-
-number = ""
-in_number = false
-this_number_is_part_number = false
+def parts_around(all_parts, ri, ci)
+  all_parts[ri-1..ri+1].flat_map do |row|
+    parts =row.find_all do | part |
+      part[1].intersection((ci-1..ci+1).to_a).length > 0  # part columns occupied intersection with our search columns
+    end
+    parts.map(&:first)
+  end
+end
 
 sum = 0
-
-field.each_with_index do | row, row_idx |
-  row.each_with_index do | value, column_idx |
-    # puts("(#{row_idx}, #{column_idx}) => #{value}")
-    if (is_digit?(value))
-        if (!in_number)
-          in_number = true
-        end
-        if (!this_number_is_part_number && is_part_no(field, row_idx, column_idx))
-          this_number_is_part_number = true
-        end
-        number += value
-    else
-      if in_number
-        if this_number_is_part_number
-          sum += number.to_i
-        end
-        in_number = false
-        number = ""
-        this_number_is_part_number = false
-      end
+raw_field.each_with_index do | row, row_index |
+  # puts row
+  row.scan('*') do
+    # puts ("Got one at #{row_index},#{Regexp.last_match.offset(0)[0]}")
+    parts = parts_around(all_parts, row_index, Regexp.last_match.offset(0)[0])
+    if (parts.length == 2)
+      sum += parts.map(&:to_i).reduce(:*)
     end
-  end
-  if in_number
-    if this_number_is_part_number
-      sum += number.to_i
-    end
-    in_number = false
-    number = ""
-    this_number_is_part_number = false
   end
 end
-
-puts ("-------")
 puts (sum)
